ea76b7bd0b9a690d51105b310ad1385d191ee8bc
Henry Bravo <henry.bravo89@gmail.com>
2018-11-18 08:17:25 -0500

    Init project, and create admin component base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './modules/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule
